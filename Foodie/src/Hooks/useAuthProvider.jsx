fb6ba4f3f11618a4ae52956221c44810aed5f10b
Md. Hossain Ahamed <89093751+Hossain-Ahamed@users.noreply.github.com>
2023-12-30 22:19:33 +0600

    Delete Foodie directory

--- Foodie/src/Hooks/useAuthProvider.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../Contexts/AuthProvider/AuthProvider';
-
-const useAuthProvider = () => {
-    const auth = useContext(AuthContext);
-    return auth;
-};
-
-export default useAuthProvider;
