2c9ab52fbd200cb9583894afe5b8eb69dadf1d76
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2024-01-01 12:17:17 +0600

    mobile navbar menu icon and company name added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
+import ScrollToTop from '../../components/ScrollToTop/ScrollToTop';
+import Navbar from '../../components/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
-            This is home page
-            <SectionTitle h1='Hello gelo'/>
+            <ScrollToTop />
+            {/* This is home page
+            <SectionTitle h1='Hello gelo'/> */}
+            <Navbar />
         </div>
     );
 };
