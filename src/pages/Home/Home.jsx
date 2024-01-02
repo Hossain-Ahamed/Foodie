8a9b7bfa5adeb68ba63d88d9f95b9d1f7d46011f
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2024-01-02 14:04:50 +0600

    landing page completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
-import SectionTitle from '../../components/SectionTitle/SectionTitle';
 import ScrollToTop from '../../components/ScrollToTop/ScrollToTop';
-import Navbar from '../../components/Navbar/Navbar';
-
+import Header from '../Shared/Header/Header.jsx';
+import Banner from '../../components/LandingPageBanner/Banner.jsx';
+import Divider from '../../components/LandingPageDivider/Divider.jsx';
+import Feature from '../../components/LandingPageFeatureSection/Feature.jsx';
+import home2 from '../../assets/images/Home/home2.png';
+import Pricing from '../../components/LandingPagePricing/Pricing.jsx';
 const Home = () => {
     return (
         <div>
             <ScrollToTop />
-            {/* This is home page
-            <SectionTitle h1='Hello gelo'/> */}
-            <Navbar />
+            {/* this is navbar */}
+            <Header />
+
+            {/* this is banner image under navbar */}
+            <Banner />
+
+            {/* this is the section under banner image */}
+            <Divider />
+
+            <Feature
+                img={home2}
+                flexDirection={false}
+                background={true}
+                responsiveDirection='flex-row'
+                heading='OUR SOFTWARE FEATURES'
+            />
+            <Feature
+                img={home2}
+                flexDirection={true}
+                background={false}
+                responsiveDirection='flex-row-reverse'
+                heading='OUR SOFTWARE FEATURES'
+            />
+
+            <Pricing />
+
+            <Feature
+            img={home2}
+            heading='Download Our Software'
+            />
         </div>
     );
 };
