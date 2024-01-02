306c28afa29f7140dec52bc995991e103f2107df
Farhan-Hasan-Nilok <nilok8679@gmail.com>
2024-01-02 14:28:32 +0600

    2:28pm 1/2/24

--- src/components/LandingPageFeatureSection/Feature.jsx
+++ src/components/LandingPageFeatureSection/Feature.jsx
@@ -5,7 +5,7 @@ import { FaAngleRight } from "react-icons/fa";
 const Feature = ({ img, flexDirection, background, responsiveDirection, heading }) => {
     return (
         <div className={`max-w-screen-xl p-3 my-8 flex mx-auto flex-col items-center gap-5 ${flexDirection ? 'md:flex-row-reverse' : 'md:flex-row'} ${background ? 'bg-gray-100' : ''}`}>
-            {/* this is for text  */}
+            {/* this is for text section */}
             <div>
                 <div>
                     <SectionTitle
